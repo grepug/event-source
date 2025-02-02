9573201033c457f0786e4419fea8752bde0a52c1
Kai Shao <grepug@icloud.com>
2025-02-02 19:24:06 +0800

    rename package to use lowercase naming convention

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "EventSource",
+    name: "event-source",
 	platforms: [.iOS(.v16), .macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
